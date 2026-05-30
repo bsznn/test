5ef12e9d67a1f89dab5f8c4484ff84e3bc6e4698
Suzan <suzan@MacBook-Air-de-Suzan.local>
2026-05-30 15:15:16 +0200

    fix index microservices

--- gateway/routes/notifi.js
+++ gateway/routes/notifi.js
@@ -1,14 +1,19 @@
 // gateway/routes/notifi.js
 const express = require('express');
 const proxy = require('express-http-proxy');
-require('dotenv').config();
 
 const router = express.Router();
 
-const NOTIFI_SERVICE_URL = process.env.NOTIFI_SERVICE_URL;
+router.use('/', (req, res, next) => {
+  const target = process.env.NOTIFI_SERVICE_URL;
 
-router.use('/', proxy(NOTIFI_SERVICE_URL, {
-  proxyReqPathResolver: (req) => req.originalUrl,
-}));
+  if (!target) {
+    return res.status(500).json({ error: 'NOTIFI_SERVICE_URL not defined' });
+  }
+
+  proxy(target, {
+    proxyReqPathResolver: (req) => req.originalUrl,
+  })(req, res, next);
+});
 
 module.exports = router;
--- gateway/routes/stock.js
+++ gateway/routes/stock.js
@@ -1,13 +1,19 @@
 // gateway/routes/stock.js
 const express = require('express');
 const proxy = require('express-http-proxy');
-require('dotenv').config();
+
 const router = express.Router();
 
-const STOCK_SERVICE_URL = process.env.STOCK_SERVICE_URL;
+router.use('/', (req, res, next) => {
+  const target = process.env.STOCK_SERVICE_URL;
 
-router.use('/', proxy(STOCK_SERVICE_URL, {
-  proxyReqPathResolver: (req) => req.originalUrl,
-}));
+  if (!target) {
+    return res.status(500).json({ error: 'STOCK_SERVICE_URL not defined' });
+  }
+
+  proxy(target, {
+    proxyReqPathResolver: (req) => req.originalUrl,
+  })(req, res, next);
+});
 
 module.exports = router;
